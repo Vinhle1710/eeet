130a69813ee2437ea5a9bc8d533718e8bc38141b
Vinhle1710 <117881066+Vinhle1710@users.noreply.github.com>
2023-12-01 14:17:49 +0700

    m

---
+++ EEET2601_lab/w5_lab/Question1.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#define SIZE 100
+
+int main() {
+    char str[SIZE]; //store maximum 99 characters
+
+    printf("Enter string: ");
+    scanf("%[^\n]s", str);
+    
+    int count = 0;
+    int sum = 0;
+
+    for (int i = 0; str[i] != '\0'; i++){
+        if ( isalpha(str[i]) ) {
+
+            count ++; 
+            
+            if ( islower(str[i]) )
+            {
+
+                str[i] = toupper(str[i]);
+
+            } else {
+
+                str[i] = tolower(str[i]);
+
+            }
+            
+        } else if ( isdigit (str[i]) ) {
+
+            sum += (str[i] - 48) ;
+
+        }
+    }
+
+    printf("Number of alphabet characters: %d \n", count);
+    printf("Converted string: %s\n", str);
+    printf("Sum of digits: %d \n", sum);
+
+    return 0;
+}
